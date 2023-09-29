842bf33bc61291f6e25767b1b3c38a0d84b7810f
Kunhee Ha <kunhee@bloommoney.co>
2023-09-29 17:25:56 +0100

    add extra for raycast

--- scripts/rotate.sh
+++ scripts/rotate.sh
@@ -1,4 +1,14 @@
 #!/bin/zsh
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Rotate
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Rotate LG Dualup Monitor
 
 CURRENT=$(displayplacer list | grep 'Rotation' | awk '{print $2}')
 ID=$(displayplacer list | grep 'Persistent' | awk '{print $4}')
