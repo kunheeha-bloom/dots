3e298cd253d7835430815f0b317556412d5d9261
Kunhee Ha <kunhee@bloommoney.co>
2024-08-21 10:23:51 +0100

    explicitly set current tty as gpg_tty

--- aliases/work.zsh
+++ aliases/work.zsh
@@ -1,2 +1,6 @@
 # ECR Docker login
 alias ecrlogin="aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 585600980607.dkr.ecr.eu-west-2.amazonaws.com"
+# TrueLayer CockroachDB connect
+alias cockroach-tl="cockroach sql --url \"postgresql://kunheeha@sugar-snapper-5512.6zw.aws-eu-west-1.cockroachlabs.cloud:26257/truelayer_transactions?sslmode=verify-full\""
+# Set gpg_tty to current
+alias sign="export GPG_TTY=$(tty)"
